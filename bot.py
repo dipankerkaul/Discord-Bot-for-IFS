9b7342389d4619d382fa475ae83df93994bf1995
dipankerkaul <42276172+dipankerkaul@users.noreply.github.com>
2023-03-07 17:03:39 +0530

    Added the bot to the discord server with Authorization URL, and was now able to print the name of the server and a list of its current members

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 
-# bot.py
+
 # bot.py
 
 
@@ -10,12 +10,26 @@ import os
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client(intents=discord.Intents.default())
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+
+
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
 
 client.run(TOKEN)
 
